857a8c9fc885036a9f289481c1d317e4a6792b2e
12792 <ronitdas033@gmail.com>
2026-05-14 17:34:14 +0530

    feat: add login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt=require("jsonwebtoken")
+const emailService = require("../services/email.service")
 
 
 
@@ -28,7 +29,7 @@ async function userRegisterController(req,res){
     })
 
     const token =jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
-    res.cookies("token",token)
+    res.cookie("token",token)
     res.status(201)
     res.status(201).json({
         user:{
@@ -39,8 +40,45 @@ async function userRegisterController(req,res){
         token
     })
 
+    await emailService.sendRegistrationEmail(user.email, user.name)
+
+}
+/**
+ *  -User Login Controller
+ * - POST /api/auth/login
+ * */
+
+async function userLoginController(req,res){
+    const {email , password }=req.body
+    const user = await userModel.findOne({ email }).select("+password")
+
+    if(!user){
+        return res.status(401).jsom({
+            message:"Email or password is INVALID"
+        })
+    }
+    const isValidPassword= await user.comparePassword(password)
+
+    if(!isValidPassword){
+         return res.status(401).jsom({
+            message:"Email or password is INVALID"
+        })
+    }
+    /*response back*/
+     const token =jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
+    res.cookie("token",token)
+    
+    res.status(200).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    })
 }
 
 module.exports={
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
